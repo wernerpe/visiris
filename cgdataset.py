0547b0be649b01312df8807a33bbc57a8a16e3c9
Peter <wernerpe@mit.edu>
2023-04-12 14:44:17 -0400

    cg ds added

--- cgdataset.py
+++ cgdataset.py
@@ -1,3 +1,3 @@
 #use this to load cg dataset
-
+#https://cgshop.ibr.cs.tu-bs.de/competition/cg-shop-2023/#problem-description
 #need to figure out how to run iris in those polygons - maybe treat linesegments as polytopes and add as obstacles?
