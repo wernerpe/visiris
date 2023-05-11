a4f5cae5ed4a9856943080e6b08336a989785520
cohnt <cohnt@umich.edu>
2023-05-11 16:48:30 -0400

    Little script to examine the behavior of the size of the maximum independent set as the number of points increases.

---
+++ convergence_test.py
@@ -0,0 +1,23 @@
+import numpy as np
+from visibility_graphs import get_visibility_graph
+from independent_set import solve_max_independent_set_integer
+import matplotlib.pyplot as plt
+from cgdataset import World
+
+world_name = "cheese132.instance.json"
+world = World("./data/examples_01/" + world_name)
+n = 1000
+seed = 0
+
+points, adj_mat, _, _, _ = get_visibility_graph(world_name, world, n, seed)
+
+step = 100
+set_size = []
+for i in range(0, n+step, step):
+	count, _ = solve_max_independent_set_integer(adj_mat[:i,:i])
+	set_size.append(count)
+
+plt.plot(np.arange(0, n+step, step), set_size)
+plt.xlabel("Number of Points")
+plt.ylabel("Maximum Independent Set Size")
+plt.show()
