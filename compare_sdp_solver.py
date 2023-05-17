9eb2d35c19764261039844bb2a07030b197908c1
cohnt <cohnt@umich.edu>
2023-05-17 16:12:06 -0400

    Measure memory usage.

--- compare_sdp_solver.py
+++ compare_sdp_solver.py
@@ -4,6 +4,7 @@ from independent_set import solve_max_independent_set_binary_quad_GW
 import matplotlib.pyplot as plt
 from cgdataset import World
 from pydrake.all import MosekSolver, ScsSolver
+import resource
 
 world_name = "srpg_iso_aligned_mc0000172.instance.json"
 world = World("./data/examples_01/" + world_name)
@@ -13,10 +14,12 @@ seed = 0
 points, adj_mat, _, _, _ = get_visibility_graph(world_name, world, n, seed)
 adj_mat = adj_mat.toarray()
 
-num = 300
+num = 200
 
-solver_names = ["Mosek", "SCS"]
-solvers = [MosekSolver(), ScsSolver()]
+solver_names = ["SCS", "MOSEK"]
+solvers = [ScsSolver(), MosekSolver()]
 for solver_name, solver in zip(solver_names, solvers):
 	count, _ = solve_max_independent_set_binary_quad_GW(adj_mat[:num,:num], sdp_solver=solver)
 	print("%s: %d" % (solver_name, count))
+	print("Memory usage: %d" % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+	# This is the overall maximum, but we know SCS will use less, so this will work
